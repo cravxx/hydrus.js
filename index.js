fde6a77ad8a2eb7d8e4ffd49eace8d2ccfe7c2ad
cravxx <environskrakow@protonmail.com>
2019-02-11 17:57:14 -0600

    eslint

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-"use strict";
-
 const rp = require('request-promise');
 
-const default_api_address = "http://127.0.0.1:45869"
+const default_api_address = 'http://127.0.0.1:45869';
 
 const ENDPOINTS = {
-  API_VERSION: "/api_version",
-  VERIFY_KEY: "/verify_access_key",
-  URL_INFO: "/add_urls/get_url_info",
-  ADD_URL: "/add_urls/add_url",
-  REQUEST_PERMISSIONS: "/request_new_permissions",
-  TAG_SERVICES: "/add_tags/get_tag_services",
-  FILE_SEARCH: "",
-  FILE_METADATA: "",
-  FILE: "",
-  THUMBNAIL: "",
-  ADD_TAGS: "",
-  ADD_FILE: "/add_files/add_file"
-}
+  API_VERSION: '/api_version',
+  VERIFY_KEY: '/verify_access_key',
+  URL_INFO: '/add_urls/get_url_info',
+  ADD_URL: '/add_urls/add_url',
+  REQUEST_PERMISSIONS: '/request_new_permissions',
+  TAG_SERVICES: '/add_tags/get_tag_services',
+  FILE_SEARCH: '',
+  FILE_METADATA: '',
+  FILE: '',
+  THUMBNAIL: '',
+  ADD_TAGS: '',
+  ADD_FILE: '/add_files/add_file',
+};
 
 const FILE_STATUS = {
   NOT_IN_DATABASE: 0,
@@ -25,44 +23,40 @@ const FILE_STATUS = {
   ALREADY_IN_DATABASE: 2,
   PREVIOUSLY_DELETED: 3,
   FAILED: 4,
-  VETOED: 7
-}
+  VETOED: 7,
+};
 
 const URL_TYPE = {
   POST_URL: 0,
   FILE_URL: 2,
   GALLERY_URL: 3,
   WATCHABLE_URL: 4,
-  UNKNOWN_URL: 5
-}
+  UNKNOWN_URL: 5,
+};
 
 const PERMISSIONS = {
   IMPORT_FILES: 0,
   ADD_TAGS: 1,
   IMPORT_URLS: 2,
-  SEARCH_FILES: 3
-}
+  SEARCH_FILES: 3,
+};
 
 const isJSON = (str, callback) => {
   try {
-      callback (JSON.parse(str));
+    callback(JSON.parse(str));
   } catch (e) {
-      callback (null);
+    callback(null);
   }
-}
+};
 
 module.exports = class Client {
-
-  constructor({
-    address = default_api_address, 
-    key = ''
-  }){
+  constructor({address = default_api_address, key = ''}) {
     this.access_key = key;
     this.address = address;
   }
 
   get IMPORT_STATUS() {
-    return FILE_STATUS
+    return FILE_STATUS;
   }
 
   get URL_TYPE() {
@@ -73,150 +67,184 @@ module.exports = class Client {
     return PERMISSIONS;
   }
 
-  build_call(method, endpoint, callback, options={}) {
-    if(this.access_key != '' && !('headers' in options))
-      options["headers"] = { 'Hydrus-Client-API-Access-Key': this.access_key }
+  build_call(method, endpoint, callback, options = {}) {
+    if (this.access_key !== '' && !('headers' in options))
+      options['headers'] = {'Hydrus-Client-API-Access-Key': this.access_key};
     rp({
       method: method,
       uri: this.address + endpoint,
-      headers: options["headers"],
-      qs: options["queries"],
-      json: options["json"]     
-    }).then(function (response) {
-      callback(null, response)
-    }).catch(function (err) {
-      callback(err, null);
-    });
+      headers: options['headers'],
+      qs: options['queries'],
+      json: options['json'],
+    })
+      .then(function(response) {
+        callback(null, response);
+      })
+      .catch(function(err) {
+        callback(err, null);
+      });
   }
 
   get_permissions(name, permissions, callback) {
-    var options = {};
-    options["queries"] = {
-      'name': name,
-      'basic_permissions': JSON.stringify(permissions)
+    var options = {
+      queries: {
+        name: name,
+        basic_permissions: JSON.stringify(permissions),
+      },
     };
-    this.build_call('GET', ENDPOINTS.REQUEST_PERMISSIONS, function(err, body) {
-      if(err) {
-        console.log(`API Error: ${JSON.stringify(err)}`)
-      } else {
-        isJSON(body, function(result) {          
-          if(result != null){
-            callback(result)
-          }else{
-            callback(body)
-          }
-        });
-      }    
-    }, options);
+    this.build_call(
+      'GET',
+      ENDPOINTS.REQUEST_PERMISSIONS,
+      function(err, body) {
+        if (err) {
+          console.log(`API Error: ${JSON.stringify(err)}`);
+        } else {
+          isJSON(body, function(result) {
+            if (result != null) {
+              callback(result);
+            } else {
+              callback(body);
+            }
+          });
+        }
+      },
+      options
+    );
   }
 
-  verify_access_key(callback, key='') {
-    var options = key != '' ? {"headers" : {'Hydrus-Client-API-Access-Key': key}} : {};
-    this.build_call('GET', ENDPOINTS.VERIFY_KEY, function(err, body) {
-      if(err) {
-        console.log(`API Error: ${JSON.stringify(err)}`)
-      } else {
-        isJSON(body, function(result) {          
-          if(result != null){
-            callback(result)
-          }else{
-            callback(body)
-          }
-        });
-      }    
-    }, options);
+  verify_access_key(callback, key = '') {
+    var options =
+      key !== '' ? {headers: {'Hydrus-Client-API-Access-Key': key}} : {};
+    this.build_call(
+      'GET',
+      ENDPOINTS.VERIFY_KEY,
+      function(err, body) {
+        if (err) {
+          console.log(`API Error: ${JSON.stringify(err)}`);
+        } else {
+          isJSON(body, function(result) {
+            if (result != null) {
+              callback(result);
+            } else {
+              callback(body);
+            }
+          });
+        }
+      },
+      options
+    );
   }
 
   get_tag_services(callback) {
     this.build_call('GET', ENDPOINTS.TAG_SERVICES, function(err, body) {
-      if(err) {
-        console.log(`API Error: ${JSON.stringify(err)}`)
+      if (err) {
+        console.log(`API Error: ${JSON.stringify(err)}`);
       } else {
-        isJSON(body, function(result) {          
-          if(result != null){
-            callback(result)
-          }else{
-            callback(body)
+        isJSON(body, function(result) {
+          if (result != null) {
+            callback(result);
+          } else {
+            callback(body);
           }
         });
-      }      
+      }
     });
   }
 
   get_url_files(url, callback) {
     var options = {};
-    options["queries"] = {'url': url};
-    this.build_call('GET', ENDPOINTS.URL_INFO, function(err, body) {
-      if(err) {
-        console.log(`API Error: ${JSON.stringify(err)}`)
-      } else {
-        isJSON(body, function(result) {          
-          if(result != null){
-            callback(result)
-          }else{
-            callback(body)
-          }
-        });
-      }      
-    }, options);
+    options['queries'] = {url: url};
+    this.build_call(
+      'GET',
+      ENDPOINTS.URL_INFO,
+      function(err, body) {
+        if (err) {
+          console.log(`API Error: ${JSON.stringify(err)}`);
+        } else {
+          isJSON(body, function(result) {
+            if (result != null) {
+              callback(result);
+            } else {
+              callback(body);
+            }
+          });
+        }
+      },
+      options
+    );
   }
 
   get_url_info(url, callback) {
     var options = {};
-    options["queries"] = {'url': url};
-    this.build_call('GET', ENDPOINTS.URL_INFO, function(err, body) {
-      if(err) {
-        console.log(`API Error: ${JSON.stringify(err)}`)
-      } else {
-        isJSON(body, function(result) {          
-          if(result != null){
-            callback(result)
-          }else{
-            callback(body)
-          }
-        });
-      }      
-    }, options);
+    options['queries'] = {url: url};
+    this.build_call(
+      'GET',
+      ENDPOINTS.URL_INFO,
+      function(err, body) {
+        if (err) {
+          console.log(`API Error: ${JSON.stringify(err)}`);
+        } else {
+          isJSON(body, function(result) {
+            if (result != null) {
+              callback(result);
+            } else {
+              callback(body);
+            }
+          });
+        }
+      },
+      options
+    );
   }
 
   add_file(file, callback) {
     var options = {};
-    options["json"] = {'path': file};
-    this.build_call('POST', ENDPOINTS.ADD_FILE, function(err, body) {
-      if(err) {
-        console.log(`API Error: ${JSON.stringify(err)}`)
-      } else {
-        callback(body)
-      }    
-    }, options);
+    options['json'] = {path: file};
+    this.build_call(
+      'POST',
+      ENDPOINTS.ADD_FILE,
+      function(err, body) {
+        if (err) {
+          console.log(`API Error: ${JSON.stringify(err)}`);
+        } else {
+          callback(body);
+        }
+      },
+      options
+    );
   }
 
   add_url(url, callback) {
     var options = {};
-    options["json"] = {'url': url};
-    this.build_call('POST', ENDPOINTS.ADD_URL, function(err, body) {
-      if(err) {
-        console.log(`API Error: ${JSON.stringify(err)}`)
-      } else {
-        callback(body)
-      }    
-    }, options);
+    options['json'] = {url: url};
+    this.build_call(
+      'POST',
+      ENDPOINTS.ADD_URL,
+      function(err, body) {
+        if (err) {
+          console.log(`API Error: ${JSON.stringify(err)}`);
+        } else {
+          callback(body);
+        }
+      },
+      options
+    );
   }
 
   api_version(callback) {
     this.build_call('GET', ENDPOINTS.API_VERSION, function(err, body) {
-      //will return json in the future
-      if(err) {
-        console.log(`API Error: ${JSON.stringify(err)}`)
+      //  will return json in the future
+      if (err) {
+        console.log(`API Error: ${JSON.stringify(err)}`);
       } else {
-        isJSON(body, function(result) {          
-          if(result != null){
-            callback(result)
-          }else{
-            callback(body)
+        isJSON(body, function(result) {
+          if (result != null) {
+            callback(result);
+          } else {
+            callback(body);
           }
         });
-      }    
+      }
     });
   }
-}
+};
--- test.js
+++ test.js
@@ -1,29 +1,31 @@
-var hydrus_api = require("./index.js");
+var Hydrus_Api = require('./index.js');
 
 const readline = require('readline').createInterface({
   input: process.stdin,
-  output: process.stdout
-})
+  output: process.stdout,
+});
 
-readline.question(`Enter an api access key to perform tests: `, (key) => {
-  
-  var c = new hydrus_api({key: "eb79e9e2a40dddd159299408db072ed6e6573d488b50db499f70a211ba1fa1da"})
-  var P = c.PERMISSIONS
-  var U = c.URLTYPE
-  var I = c.IMPORT_STATUS
+readline.question('Enter an api access key to perform tests: ', key => {
+  var C = new Hydrus_Api({
+    key: 'eb79e9e2a40dddd159299408db072ed6e6573d488b50db499f70a211ba1fa1da',
+  });
+  var P = C.PERMISSIONS;
+  // var U = C.URLTYPE;
+  // var I = C.IMPORT_STATUS;
 
-  // new 
+  // new
   // c.get_tag_services(function(response) {
   //   console.log(response)
   // })
 
-  // c.get_url_files("https://images-ext-2.discordapp.net/external/OFQgQQfZdll80-opJXFOKPYCG_32rjT8eQvvYWE-Zxs/http/i.4cdn.org/g/1549666714889.jpg", function(response) {
+  // c.get_url_files("https://images-ext-2.discordapp.net/external/OFQgQQfZdll80-opJXFOKPYCG_32rjT8eQvvYWE-Zxs/http/i.4cdn.org/g/1549666714889.jpg",
+  // function(response) {
   //   console.log(response)
   // })
 
-  c.api_version(function(response) {
-    console.log(response)
-  })
+  C.api_version(function(response) {
+    console.log(response);
+  });
 
   // backend on these seems to be not working
   // c.verify_access_key(function(response) {
@@ -34,17 +36,27 @@ readline.question(`Enter an api access key to perform tests: `, (key) => {
   //   console.log(response)
   // }, "f03b3d78c04f72ef18807d57f85fce71016c356c15b59d98bb76bc5870ec8bef")
 
-  c.add_url("https://images-ext-2.discordapp.net/external/OFQgQQfZdll80-opJXFOKPYCG_32rjT8eQvvYWE-Zxs/http/i.4cdn.org/g/1549666714889.jpg", function(response) {
-    console.log(response)
-  })
+  C.add_url(
+    'https://images-ext-2.discordapp.net/external/OFQgQQfZdll80-opJXFOKPYCG_32rjT8eQvvYWE-Zxs/http/i.4cdn.org/g/1549666714889.jpg',
+    function(response) {
+      console.log(response);
+    }
+  );
 
-  c.get_url_info("https://images-ext-2.discordapp.net/external/OFQgQQfZdll80-opJXFOKPYCG_32rjT8eQvvYWE-Zxs/http/i.4cdn.org/g/1549666714889.jpg", function(response) {
-    console.log(response)
-  })
+  C.get_url_info(
+    'https://images-ext-2.discordapp.net/external/OFQgQQfZdll80-opJXFOKPYCG_32rjT8eQvvYWE-Zxs/http/i.4cdn.org/g/1549666714889.jpg',
+    function(response) {
+      console.log(response);
+    }
+  );
 
-  c.get_permissions("new perms", [P.ADD_TAGS, P.IMPORT_URLS, P.IMPORT_FILES, P.SEARCH_FILES], function(response) {
-    console.log(response)
-  })
+  C.get_permissions(
+    'new perms',
+    [P.ADD_TAGS, P.IMPORT_URLS, P.IMPORT_FILES, P.SEARCH_FILES],
+    function(response) {
+      console.log(response);
+    }
+  );
 
-  readline.close()
-})
+  readline.close();
+});
